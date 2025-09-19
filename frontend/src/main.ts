d05f49ac60bd4bfb927f1680f249af289c4b0b6d
Alin Nechita <alin.nechita20@gmail.com>
2025-09-19 16:53:18 +0200

    Update todo list

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -11,11 +11,11 @@ app.use(router)
 
 app.mount('#app')
 
-// TODO: remove vuetify for good
 // TODO: make first and last name required in welcome step onboarding
 // TODO: make skip button work in onboarding steps
 // TODO: quick overview of the whole project for refactoring/optimization
 // TODO: do something after list has been added
+// TODO: create list
 // TODO: make list title required
 // TODO: display lists
 // TODO: edit lists after creation
